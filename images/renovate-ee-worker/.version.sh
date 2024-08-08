3b31755e473c73267b6c3c77656917dab450943d
Jacob McSwain <jacob@mcswain.dev>
2024-08-08 13:37:01 -0500

    version renovate images

---
+++ images/renovate-ee-worker/.version.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# renovate: datasource=docker depName=ghcr.io/mend/renovate-ee-worker
+export VERSION=7.6.1
---
+++ images/renovate-ee/.version.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# renovate: datasource=docker depName=ghcr.io/mend/renovate-ee-server
+export VERSION=7.6.1
