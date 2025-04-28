eca2c9e91a7b54004bd0713329e35dc512050eed
Jacob McSwain <jacob.a.mcswain@gmail.com>
2025-04-28 10:48:32 -0500

    indi: version: set versioning

--- images/indi-base/.version.sh
+++ images/indi-base/.version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# renovate: datasource=github-releases depName=indilib/indi
+# renovate: datasource=github-releases versioning=regex:^v(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)(\.(?<build>\d+))?$ depName=indilib/indi
 export VERSION=v2.1.2
