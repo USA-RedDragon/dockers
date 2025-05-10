69b0e3947f46bc1c6154380725c65e8ff0336d8d
Jacob McSwain <jacob@mcswain.dev>
2025-05-09 23:23:22 -0500

    mindustry: try to fix renovate

--- images/mindustry/.version.sh
+++ images/mindustry/.version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# renovate: datasource=github-releases extractVersion=^v(?<version>.*)$ versioning=regex depName=Anuken/Mindustry
+# renovate: datasource=github-releases depName=Anuken/Mindustry
 export VERSION=v145.1
